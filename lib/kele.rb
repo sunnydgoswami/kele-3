dfce5aa01d56479a74901fc1b9cf8f3c63ef816c
SunnyGoswami <sunnydgoswami@gmail.com>
2017-09-05 19:18:44 +0000

    mentor availability reciving json parse 743 error

--- lib/kele.rb
+++ lib/kele.rb
@@ -16,4 +16,8 @@ class Kele
         @user_data = JSON.parse(response.body)
     end
     
+    def get_mentor_availability(mentor_id)
+        response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { 'authorization' => @auth_token })
+        @mentor_availability = JSON.parse(response.body)
+    end
 end
