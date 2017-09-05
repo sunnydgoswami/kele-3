e1019b2da8624f131211026400d9abb8ce43bdb3
SunnyGoswami <sunnydgoswami@gmail.com>
2017-09-05 19:24:18 +0000

    roadmap

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,8 +1,10 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
 
 class Kele
     include HTTParty
+    include Roadmap
     base_uri 'https://www.bloc.io/api/v1'
     
     def initialize(email, password)
@@ -13,11 +15,12 @@ class Kele
     
     def get_me
         response = self.class.get('/users/me', headers: { 'authorization' => @auth_token })
-        @user_data = JSON.parse(response.body)
+        JSON.parse(response.body)
     end
     
     def get_mentor_availability(mentor_id)
         response = self.class.get("/mentors/#{mentor_id}/student_availability", headers: { 'authorization' => @auth_token })
-        @mentor_availability = JSON.parse(response.body)
+        JSON.parse(response.body)
     end
+    
 end
