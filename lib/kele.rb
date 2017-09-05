53fc9a1da13161afbb4cda81e40d4efc9166145f
SunnyGoswami <sunnydgoswami@gmail.com>
2017-09-05 19:40:51 +0000

    create messages and get messages

--- lib/kele.rb
+++ lib/kele.rb
@@ -23,4 +23,14 @@ class Kele
         JSON.parse(response.body)
     end
     
+    def get_messages(page = nil)
+        response = self.class.get("/message_threads", body: {page: page}, headers: {"authorization" => @auth_token})
+        JSON.parse(response.body)
+    end
+    
+    def create_message(sender, recipient_id, subject, text)
+        response = self.class.post("/messages", body: {"sender": @email, "recipient_id": recipient_id, "subject": subject, "stripped-text": text}, headers: {authorization: @auth})
+        JSON.parse(response.body)
+    end    
+    
 end
